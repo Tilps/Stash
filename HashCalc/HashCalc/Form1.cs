8b45b761a462cbe69d76101038f0fe85a543e926
Tilps <tilps@users.noreply.github.com>
2015-07-26 21:14:44 +1000

    Add a very basic SHA1 file hash calculator
    Loads file into memory, so not useful for large files.
    Generally not very useful at all :P

---
+++ HashCalc/HashCalc/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace HashCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                SHA1 sha = SHA1.Create();
+                byte[] hash = sha.ComputeHash(File.ReadAllBytes(dialog.FileName));
+                MessageBox.Show(ToString(hash));
+            }
+        }
+
+        private string ToString(byte[] hash)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in hash)
+                builder.Append(b.ToString("X2"));
+            return builder.ToString();
+        }
+    }
+}
---
+++ HashCalc/README.md
@@ -0,0 +1,2 @@
+# Hash calc
+A very simple program to calculate SHA1 of a file and display it as hex.
